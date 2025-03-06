367d57b3473f91d1cb88d7719a422738b5f303a9
marijnpaulsen <marijn.paulsen@vodafoneziggo.com>
2025-03-06 17:08:42 +0100

    Les 10 huiswerk tegevoegd

---
+++ boekhouding.py
@@ -0,0 +1,21 @@
+import csv
+
+from presentatie import *
+from helper import *
+
+
+inkomsten = {
+    "Aardbeien-ijs-totaal": 1000,
+    "Vanille-ijs-totaal": 2000,
+    "Chocolade-ijs-totaal": 1500,
+    "Waterijsjes-totaal": 750
+}
+
+totaal_inkomsten = som(inkomsten)
+
+presenteer(inkomsten, totaal_inkomsten)
+
+with open('boekhouding.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=';')
+    for key, value in inkomsten.items():
+        writer.writerow([key, value])
